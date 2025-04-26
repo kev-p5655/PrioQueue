d6462b7910411eb941ae9ea1ce2c5f2c32c47384
kev-p5655 <kevinkp5655@gmail.com>
2025-04-26 10:31:48 -0400

    Added priority to the table, and some extra helpers

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+	"strings"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -13,6 +14,7 @@ const JOB_TABLE_NAME string = "jobs"
 type Job struct {
 	id          int
 	description string
+	priority    int
 }
 
 func createDb() (*sql.DB, error) {
@@ -25,7 +27,8 @@ func createTables(db *sql.DB) error {
 	var init string = fmt.Sprintf(`
 		CREATE TABLE IF NOT EXISTS %s (
 		id INTEGER NOT NULL PRIMARY KEY,
-		description TEXT
+		description TEXT,
+		priority INTEGER NOT NULL
 		);`,
 		JOB_TABLE_NAME,
 	)
@@ -45,27 +48,65 @@ func initDb() (*sql.DB, error) {
 	return db, err
 }
 
-func createJobInsertQuery() (query string) {
+func createJobInsertQuery(db *sql.DB) (query string, err error) {
+	// Create the elements to insert
+	descriptions := []string{"hello", "goodbye"}
+	currPrio, err := getCurrPrio(db)
+	if err != nil {
+		return
+	}
+
+	currPrio++
+	fmt.Println(descriptions)
+	items := []string{}
+	for i, description := range descriptions {
+		fmt.Println(description)
+		items = append(items,
+			fmt.Sprintf(`("%s", %d)`, description, currPrio+i),
+		)
+	}
+
+	// Create the query
 	query = fmt.Sprintf(`
-		insert into %s
-		values (1, "hello"), (5, "goodbye")
-		`,
+		INSERT INTO %s (description, priority)
+		VALUES %s
+		;`,
+		JOB_TABLE_NAME,
+		strings.Join(items, ","),
+	)
+	return
+}
+
+func getCurrPrio(db *sql.DB) (prio int, err error) {
+	query := fmt.Sprintf(`
+		SELECT priority
+		FROM %s
+		ORDER BY priority DESC
+		LIMIT 1
+		;`,
 		JOB_TABLE_NAME,
 	)
+	err = db.QueryRow(query).Scan(&prio)
+	if err == sql.ErrNoRows {
+		// Handle ErrNoRows
+		err = nil
+		prio = 0
+	}
 	return
 }
 
 func getAllJobs(db *sql.DB) (jobs []Job, err error) {
 	query := fmt.Sprintf(`
-		select id, description
-		from %s
+		SELECT id, description, priority
+		FROM %s
+		ORDER by priority ASC
 		`,
 		JOB_TABLE_NAME,
 	)
 	rows, err := db.Query(query)
 	for rows.Next() {
 		var job Job
-		if err := rows.Scan(&job.id, &job.description); err != nil {
+		if err := rows.Scan(&job.id, &job.description, &job.priority); err != nil {
 			return jobs, err
 		}
 		jobs = append(jobs, job)
@@ -87,12 +128,22 @@ func exec(db *sql.DB, query string) (err error) {
 }
 
 func main() {
+	// Setup
 	db, err := initDb()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	err = exec(db, createJobInsertQuery())
+	defer db.Close()
+
+	// Queries
+	query, err := createJobInsertQuery(db)
+	fmt.Println(query)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	err = exec(db, query)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
